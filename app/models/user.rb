d026cd120d7c7476734b5a9f834159a419861f36
kokmraoi <kokmraoi@gmail.com>
2022-06-08 23:34:39 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :nickname
+    validates :firstname
+    validates :lastname
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :firstname_kata
+    validates :lastname_kata
+  end
+  validates :profile, presence: true
 end
