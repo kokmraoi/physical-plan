7aa726f873466acb67ca2e06e1ff31270f0a2809
kokmraoi <kokmraoi@gmail.com>
2022-06-09 22:34:24 +0900

    モデル単体テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
   def edit
   end
+
   def update
     current_user.update(user_params)
-    if
-      redirect_to root_path
+    if redirect_to root_path
     else
       render :edit
     end
   end
-  
+
   private
+
   def user_params
     params.require(:user).permit(:nickname, :email)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :nickname
     validates :firstname
@@ -13,5 +16,6 @@ class User < ApplicationRecord
     validates :firstname_kata
     validates :lastname_kata
   end
+  validates :birthday, presence: true
   validates :profile, presence: true
 end
