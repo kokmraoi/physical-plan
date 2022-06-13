05e1a0e99dd5a7e285b4676ff4c3b53a67a7f15f
kokmraoi <kokmraoi@gmail.com>
2022-06-13 21:25:20 +0900

    カルテ作成機能の実装

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,4 +1,7 @@
 class Chart < ApplicationRecord
   has_many :chart_users
   has_many :users, through: :chart_users
+
+  validates :name, presence: true
+
 end
--- app/models/chart_user.rb
+++ app/models/chart_user.rb
@@ -1,4 +1,4 @@
 class ChartUser < ApplicationRecord
-  belongs_to :user
   belongs_to :chart
+  belongs_to :user
 end
